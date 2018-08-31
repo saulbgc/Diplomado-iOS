358aabc9b94f232425433398830b9adb43892b7a
Saul G <42055393+saulbgc@users.noreply.github.com>
2018-08-31 14:35:29 -0500

    Add files via upload

---
+++ ENTREGABLES/RPS/ISpy/ISpy/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Saul G on 30/08/18.
+//  Copyright © 2018 SaulG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scrollView.delegate = self
+        updateZoomFor(size: view.bounds.size)
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor (size : CGSize){
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        
+    }
+
+
+}
+
